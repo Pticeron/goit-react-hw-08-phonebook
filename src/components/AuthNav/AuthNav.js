4527ad4d308d83d4b1a780e7aeb8fbcc54b62eca
Inna Zorina <111115742+Pticeron@users.noreply.github.com>
2023-06-20 19:37:55 +0300

    ,

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -4,10 +4,16 @@ import css from './AuthNav.module.css';
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink className={css.link} to="/register">
+      <NavLink 
+      className={css.link}
+      activeClassName={css.activeLink} 
+      to="/register">
         Register
       </NavLink>
-      <NavLink className={css.link} to="/login">
+      <NavLink 
+      className={css.link} 
+      activeClassName={css.activeLink}
+      to="/login">
         Log In
       </NavLink>
     </div>
